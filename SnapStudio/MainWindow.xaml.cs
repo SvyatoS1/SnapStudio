38b8d8939294ac50bc56b9ac01152ae9f9120760
Sviatoslav Simchuk <simvhuksvyatoslav@gmail.com>
2024-06-06 01:39:36 +0300

    Implement invertion

--- SnapStudio/MainWindow.xaml.cs
+++ SnapStudio/MainWindow.xaml.cs
@@ -213,7 +213,23 @@ namespace SnapStudio
 
         private void Invert_item_Click_1(object sender, RoutedEventArgs e)
         {
-            
+            try
+            {
+                if (bitmapList.Count > 0)
+                {
+                    currentPicture = bitmapList[currentBitmap];
+                    currentPicture = MatrixConvertBitmap(currentPicture, invertConMatrix);
+                    addPicture(currentPicture);
+                }
+                else
+                {
+                    MessageBox.Show("No Picture, please open a a picture to edit it");
+                }
+            }
+            catch (Exception except)
+            {
+                MessageBox.Show(except.Message);
+            }
         }
 
         private void Undo_item_Click_1(object sender, RoutedEventArgs e)
