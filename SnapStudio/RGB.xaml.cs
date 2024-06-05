0b4ed077846f7718784e66240e524cdb5109f20b
Sviatoslav Simchuk <simvhuksvyatoslav@gmail.com>
2024-06-06 02:04:22 +0300

    Add class RGB

--- SnapStudio/RGB.xaml.cs
+++ SnapStudio/RGB.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +20,115 @@ namespace SnapStudio
     /// <summary>
     /// Interaction logic for RGB.xaml
     /// </summary>
-    public partial class RGB : Page
+    public partial class RGB : UserControl
     {
-        public RGB()
+        private MainWindow myParentWindow;
+        private ColorDialog myColorDialog;
+        private int originalBitmapCount = new int();
+        private Bitmap previewBitmap;
+
+        private float redV;
+        private float greenV;
+        private float blueV;
+
+        public RGB(MainWindow mPW, ColorDialog cD)
         {
+            myParentWindow = mPW;
+            myColorDialog = cD;
             InitializeComponent();
+            originalBitmapCount = myParentWindow.CurrentBitmap;
         }
+
+        private ColorMatrix createColorMatrix(float rV, float gV, float bV)
+        {
+            ColorMatrix cMatrix = new ColorMatrix(
+                new float[][]
+                {
+                    new float[] {1, 0, 0, 0, 0},
+                    new float[] {0, 1, 0, 0, 0},
+                    new float[] {0, 0, 1, 0, 0},
+                    new float[] {0, 0, 0, 1, 0},
+                    new float[] {rV, gV, bV, 0, 1}
+                });
+            return cMatrix;
+        }
+
+        /// <summary>
+        /// Sets the main Bitmap permanently with the current user settings
+        /// Use only for changes that will be saved into state, i.e. the Apply
+        /// Button.
+        /// </summary>
+        private void setMainBitmap()
+        {
+            // Create the appropriate matrix
+            ColorMatrix cMatrix = createColorMatrix(redV, greenV, blueV);
+
+            // Get the current bitmap to edit
+            previewBitmap = myParentWindow.BitmapList[myParentWindow.CurrentBitmap];
+
+            // Apply the matrix to the bitmap
+            previewBitmap = myParentWindow.MatrixConvertBitmap(previewBitmap, cMatrix);
+
+            // Display the bitmap in the main window, add it to BitmapList, and increment the counter
+            myParentWindow.addPicture(previewBitmap);
+        }
+
+        /// <summary>
+        /// Sets the main Bitmap temporarily with the current user settings
+        /// Use only for previews of effects, as it isn't added to the BitmapList,
+        /// and goes away when this window is closed.
+        /// </summary>
+        private void setTempBitmap()
+        {
+            // Create the appropriate matrix
+            ColorMatrix cMatrix = createColorMatrix(redV, greenV, blueV);
+
+            // Get the current bitmap to edit
+            previewBitmap = myParentWindow.BitmapList[myParentWindow.CurrentBitmap];
+
+            // Apply the matrix to the bitmap
+            previewBitmap = myParentWindow.MatrixConvertBitmap(previewBitmap, cMatrix);
+
+            // Display the bitmap temporarily
+            myParentWindow.setTempPicture(previewBitmap);
+        }
+
+        #region event_handlers
+
+        private void Preview_btn_Click_1(object sender, RoutedEventArgs e)
+        {
+            setTempBitmap();
+        }
+
+        private void Apply_btn_Click_1(object sender, RoutedEventArgs e)
+        {
+            setMainBitmap();
+            myParentWindow.setMainPicture(myParentWindow.CurrentBitmap);
+        }
+
+        private void Cancel_btn_Click_1(object sender, RoutedEventArgs e)
+        {
+            myParentWindow.setMainPicture(originalBitmapCount);
+        }
+
+        private void RedSlider_ValueChanged_1(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            redV = ((float)RedSlider.Value / (float)100);
+            RedValue.Content = "" + redV;
+        }
+
+        private void BlueSlider_ValueChanged_1(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            blueV = ((float)BlueSlider.Value / (float)100);
+            BlueValue.Content = "" + blueV;
+        }
+
+        private void GreenSlider_ValueChanged_1(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            greenV = ((float)GreenSlider.Value / (float)100);
+            GreenValue.Content = "" + greenV;
+        }
+
+        #endregion
     }
 }
